9ec0386e40aee3acb74acc9af2ca1fbce0ef8108
JoshVComstock <josedanielveizaga@gmail.com>
2024-06-13 14:04:04 -0400

    cambios back

--- client/src/pages/convoObra.jsx
+++ client/src/pages/convoObra.jsx
@@ -1,60 +1,24 @@
 import React from "react";
-import styled, { keyframes } from 'styled-components';
+import styled, { keyframes } from "styled-components";
 import { Colors } from "../style/colores";
-import { ButtonD, Cuce, Form1, Table, Titulo, Titulo2, Titulo3 } from "../style/formulariosStyleUno";
+import {
+  ButtonD,
+  Cuce,
+  Form1,
+  Table,
+  Titulo,
+  Titulo2,
+  Titulo3,
+} from "../style/formulariosStyleUno";
 import { Link } from "react-router-dom";
 import Foot from "./admin/foot";
-
-const Card = styled.div`
-  border-radius: 30px;
-  box-shadow: rgba(0, 0, 0, 0.499) 0px 2px 4px 0px;
-  backdrop-filter: blur(20px);
-`;
-
-const CardBody = styled.div`
-  width: 100%;
-  height: 1070px; 
-  margin-top: 15px;
-`;
-const Scroll = styled.div`
-  width: 100%;
-  height: 848px; 
-  overflow-y: scroll;
-`;
-const CardBody1 = styled.div`
-  width: 100%;
-  background:${Colors.primary100};
-  box-shadow: rgba(0, 0, 0, 0.499) 0px 2px 4px 0px;
-  border-radius: 5px;
-`;
-
-const ImgOjo = styled.img`
-  width: 40px;
-  height: 40px;
-`;
-
-const Card1 = styled.div`
-  height: 100px; 
-  display: flex;
-  justify-content:center;
-  align-items: center;
-`;
-
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  padding-top: 15px;
-  h1{
-    color: ${Colors.primary500}
-  }
-`;
-const ContainerForm = styled.div`
-  width: 1519px;
-  height: 100%;
-  padding: 80px;
-`;
+import { useGetDelete } from "../hooks/useGetDelete";
+import { ENDPOINTS } from "../routes/enpoints";
+import { convertirFecha } from "../utils/dateformat";
 
 const ConvoServicios = () => {
+  const { res } = useGetDelete(ENDPOINTS.CONVOCATORIA.GET);
+
   return (
     <>
       <MovingBackground />
@@ -72,38 +36,51 @@ const ConvoServicios = () => {
                   <Scroll>
                     <thead>
                       <tr>
-                        <th >DEPARTAMENTO</th>
-                        <th >NOMBRE PROYECTO/ MODALIDAD</th>
-                        <th >MODALIDAD</th>
-                        <th >FECHA SUBIDA</th>
-                        <th ><ImgOjo src="https://c0.klipartz.com/pngpicture/453/989/gratis-png-icono-de-accion-de-lista-de-verificacion-de-iconos-de-computadora-salud-dental-y-hoja-de-registro.png" alt="" /></th>
+                        <th>Nro</th>
+                        <th>DEPARTAMENTO</th>
+                        <th>DESCRIPCION</th>
+                        <th>NOMBRE PROYECTO/ MODALIDAD</th>
+                        <th>MODALIDAD</th>
+                        <th>FECHA SUBIDA</th>
+                        <th>
+                          <ImgOjo
+                            src="https://c0.klipartz.com/pngpicture/453/989/gratis-png-icono-de-accion-de-lista-de-verificacion-de-iconos-de-computadora-salud-dental-y-hoja-de-registro.png"
+                            alt=""
+                          />
+                        </th>
                       </tr>
                     </thead>
                     <tbody>
-                      <tr>
-                        <th>Cochabamba</th>
-                        <th >Const. tinglado cancha polifuncional mzn 010 cinti raymi otb santa monica de Obras</th>
-                        <th >ANPE</th>
-                        <th>Presentación: 08 / 05 / 2024</th>
-                        <th ><ButtonD><Link to="/postular">POSTULAR</Link></ButtonD></th>
-                      </tr>
+                      {res?.map((v, i) => (
+                        <tr key={i}>
+                          <th>{i + 1}</th>
+                          <th>{v.nombre}</th>
+                          <th>{v.descripcion}</th>
+                          <th>{v.departamento}</th>
+                          <th>{v.modalidad}</th>
+                          <th>{convertirFecha(v.fechaPublicacion)}</th>
+                          <th>
+                            <ButtonD>
+                              <Link to="/postular">POSTULAR</Link>
+                            </ButtonD>
+                          </th>
+                        </tr>
+                      ))}
                     </tbody>
                   </Scroll>
                 </Table>
-                <Titulo2>
-                </Titulo2>
+                <Titulo2></Titulo2>
               </Form1>
             </CardBody>
           </Card>
         </ContainerForm>
-        <Foot/>
+        <Foot />
       </Container>
     </>
-  )
-}
+  );
+};
 
 export default ConvoServicios;
-
 
 const moveBackground = keyframes`
 0% { background-position: 0 0; }
@@ -111,13 +88,63 @@ const moveBackground = keyframes`
 `;
 
 const MovingBackground = styled.div`
-position: absolute;
-top: 0;
-left: 0;
-width: 100%;
-height: 1400px;
-background: url('https://vinccler.wordpress.com/wp-content/uploads/2020/10/vinccler-construccion-conjunto-de-tareas-y-objetivos-definidos.jpg') repeat;
-background-size:cover;
-animation: ${moveBackground} 80s linear infinite;
-z-index: -2;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 1400px;
+  background: url("https://vinccler.wordpress.com/wp-content/uploads/2020/10/vinccler-construccion-conjunto-de-tareas-y-objetivos-definidos.jpg")
+    repeat;
+  background-size: cover;
+  animation: ${moveBackground} 80s linear infinite;
+  z-index: -2;
+`;
+
+const Card = styled.div`
+  border-radius: 30px;
+  box-shadow: rgba(0, 0, 0, 0.499) 0px 2px 4px 0px;
+  backdrop-filter: blur(20px);
+`;
+
+const CardBody = styled.div`
+  width: 100%;
+  height: 1070px;
+  margin-top: 15px;
+`;
+const Scroll = styled.div`
+  width: 100%;
+  height: 848px;
+  overflow-y: scroll;
+`;
+const CardBody1 = styled.div`
+  width: 100%;
+  background: ${Colors.primary100};
+  box-shadow: rgba(0, 0, 0, 0.499) 0px 2px 4px 0px;
+  border-radius: 5px;
+`;
+
+const ImgOjo = styled.img`
+  width: 40px;
+  height: 40px;
+`;
+
+const Card1 = styled.div`
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  padding-top: 15px;
+  h1 {
+    color: ${Colors.primary500};
+  }
+`;
+const ContainerForm = styled.div`
+  width: 1519px;
+  height: 100%;
+  padding: 80px;
 `;
--- client/src/pages/proyecto.jsx
+++ client/src/pages/proyecto.jsx
@@ -1,92 +1,227 @@
-import React, { useState } from 'react'
-import { ButtonSig, ContainerForm, DivButton, Table, Th  } from '../style/formulariosStyleUno';
-import { Form, Titulo, Titulo2, FormGroup, Cuce, InputD} from '../style/formulariosStyleDosTres';
-import { Link } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
-import { toast } from 'react-toastify';
+import React, { useState } from "react";
+import {
+  ButtonSig,
+  ContainerForm,
+  DivButton,
+  Table,
+  Th,
+} from "../style/formulariosStyleUno";
+import {
+  Form,
+  Titulo,
+  Titulo2,
+  FormGroup,
+  Cuce,
+  InputD,
+} from "../style/formulariosStyleDosTres";
+import { Link } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import { toast } from "react-toastify";
+import Dos from "./proponenteObras/dos";
+import Tres from "./proponenteObras/tres";
+import Cuatro from "./proponenteObras/cuatro";
+import Cinco from "./proponenteObras/cinco";
+import Seis from "./proponenteObras/seis";
+import Siete from "./proponenteObras/siete";
+import { useUser } from "../store/user";
 
-const Proyecto = () => {
-    const [form, setForm] = useState({
-        nombre: "",
-        cuce: uuidv4(),
-        entidad: "",
-        departamento: "",
-        contacto: 0,
-        telefono:0,
-        tipoContratacion: "",
-        modalidad: "",
+const Proyecto = ({ idConvocatoria }) => {
+  const { user } = useUser();
+  const [form, setForm] = useState({
+    nombre: "",
+    cuce: uuidv4(),
+    entidad: "",
+    departamento: "",
+    contacto: 0,
+    telefono: 0,
+    tipoContratacion: "",
+    modalidad: "",
+  });
 
-    });
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-          const response = await fetch("http://localhost:5000/formulario", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(
-              {
-                descripcion: "Formulario2",
-                archivo: form
-              }
-            ),
-          });
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          toast.success("Formularios Enviados Correctamente");
-          const data = await response.json();
-        } catch (error) {
-          console.error("Error:", error);
-        }
-      };
-    return (
-        <ContainerForm>
+  const [step, setStep] = useState(1);
+
+  const obtenerFechaActual = () => {
+    const hoy = new Date();
+    const opciones = { year: "numeric", month: "long", day: "numeric" };
+    return hoy.toLocaleDateString("es-ES", opciones);
+  };
+  console.log(form);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:5000/proyecto", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          idUsuario: user.id,
+          idConvocatoria: idConvocatoria,
+          nombre: form.nombre,
+          cuce: form.cuce,
+          estado: "En proceso",
+          entidad: form.entidad,
+          departamento: form.departamento,
+          fechaPublicacion: obtenerFechaActual(),
+          fechaPresentacion: obtenerFechaActual(),
+          contacto: form.contacto,
+          telefono: form.telefono,
+          tipoContratacion: form.tipoContratacion,
+          modalidad: form.modalidad,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      toast.success("Proyecto enviado correctamente");
+      const data = await response.json();
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const renderStep = () => {
+    switch (step) {
+      case 1:
+        return (
+          <ContainerForm>
             <Form>
-                <Titulo>
-                    <label>REGISTRO DE LICITACIÓN PARA POSTULARSE A CONVOCATORIA</label>
-                    <label>(Por favor llene todos los campos)</label>
-                </Titulo>
-                <Titulo2>
-                    <label>1. DATOS DEL OBJETO DE LA CONTRATACIÓN</label>
-                </Titulo2>
-                <FormGroup>
-                    <Cuce>
-                        <div className="form-group">
-                            <label>Nombre Licitación:</label>
-                            <label>CUCE:</label>
-                            <label>Entidad:</label>
-                            <label>Departamento:</label>
-                            <label>Contacto:</label>
-                            <label>Teléfono:</label>
-                            <label>Tipo Contratación:</label>
-                            <label>Modalidad:</label>
-                        </div>
-                        <div>
-                            <InputD type="text" required value={form.nombre} onChange={(event) => setForm((old) => ({...old, nombre:event.target.value }))} />
-                            <InputD type="number" required value={form.cuce} onChange={(event) => setForm((old) => ({...old, cuce:event.target.value }))} />
-                            <InputD type="text" required value={form.entidad} onChange={(event) => setForm((old) => ({...old, entidad:event.target.value}))}/>
-                            <InputD type="text" required value={form.departamento} onChange={(event) => setForm((old) =>({...old, departamento:event.target.value}))}/>
-                            <InputD type="text" required value={form.contacto} onChange={(event) => setForm((old) =>({...old, contacto:event.target.value}))}/>
-                            <InputD type="number" required value={form.telefono} onChange={(event) => setForm((old) =>({...old, telefono:event.target.value}))}/>
-                            <InputD type="text" required value={form.tipoContratacion} onChange={(event) => setForm((old) =>({...old, tipoContratacion:event.target.value}))}/>
-                            <InputD type="text" required value={form.modalidad} onChange={(event) => setForm((old) =>({...old, modalidad:event.target.value}))}/>
-
-                        </div>
-                    </Cuce>
-                </FormGroup>
-                <Titulo2>
-                    <label></label>
-                </Titulo2>
-                <DivButton>
-                    <ButtonSig  onClick={handleSubmit}><Link to="/siguiente">SIGUIENTE</Link></ButtonSig>
-                </DivButton>
+              <Titulo>
+                <label>
+                  REGISTRO DE LICITACIÓN PARA POSTULARSE A CONVOCATORIA
+                </label>
+                <label>(Por favor llene todos los campos)</label>
+              </Titulo>
+              <Titulo2>
+                <label>1. DATOS DEL OBJETO DE LA CONTRATACIÓN</label>
+              </Titulo2>
+              <FormGroup>
+                <Cuce>
+                  <div className="form-group">
+                    <label>Nombre Licitación:</label>
+                    <label>CUCE:</label>
+                    <label>Entidad:</label>
+                    <label>Departamento:</label>
+                    <label>Contacto:</label>
+                    <label>Teléfono:</label>
+                    <label>Tipo Contratación:</label>
+                    <label>Modalidad:</label>
+                  </div>
+                  <div>
+                    <InputD
+                      type="text"
+                      required
+                      value={form.nombre}
+                      onChange={(event) =>
+                        setForm((old) => ({
+                          ...old,
+                          nombre: event.target.value,
+                        }))
+                      }
+                    />
+                    <InputD
+                      type="text"
+                      required
+                      value={form.cuce}
+                      onChange={(event) =>
+                        setForm((old) => ({ ...old, cuce: event.target.value }))
+                      }
+                      disabled
+                    />
+                    <InputD
+                      type="text"
+                      required
+                      value={form.entidad}
+                      onChange={(event) =>
+                        setForm((old) => ({
+                          ...old,
+                          entidad: event.target.value,
+                        }))
+                      }
+                    />
+                    <InputD
+                      type="text"
+                      required
+                      value={form.departamento}
+                      onChange={(event) =>
+                        setForm((old) => ({
+                          ...old,
+                          departamento: event.target.value,
+                        }))
+                      }
+                    />
+                    <InputD
+                      type="text"
+                      required
+                      value={form.contacto}
+                      onChange={(event) =>
+                        setForm((old) => ({
+                          ...old,
+                          contacto: event.target.value,
+                        }))
+                      }
+                    />
+                    <InputD
+                      type="number"
+                      required
+                      value={form.telefono}
+                      onChange={(event) =>
+                        setForm((old) => ({
+                          ...old,
+                          telefono: event.target.value,
+                        }))
+                      }
+                    />
+                    <InputD
+                      type="text"
+                      required
+                      value={form.tipoContratacion}
+                      onChange={(event) =>
+                        setForm((old) => ({
+                          ...old,
+                          tipoContratacion: event.target.value,
+                        }))
+                      }
+                    />
+                    <InputD
+                      type="text"
+                      required
+                      value={form.modalidad}
+                      onChange={(event) =>
+                        setForm((old) => ({
+                          ...old,
+                          modalidad: event.target.value,
+                        }))
+                      }
+                    />
+                  </div>
+                </Cuce>
+              </FormGroup>
+              <DivButton>
+                <ButtonSig onClick={() => setStep(2)}>SIGUIENTE</ButtonSig>
+              </DivButton>
             </Form>
-        </ContainerForm>
-    )
-}
+          </ContainerForm>
+        );
+      case 2:
+        return <Dos setStep={setStep} cuce={form.cuce} />;
+      case 3:
+        return <Tres setStep={setStep} />;
+      case 4:
+        return <Cuatro setStep={setStep} />;
+      case 5:
+        return <Cinco setStep={setStep} />;
+      case 6:
+        return <Seis setStep={setStep} />;
+      case 7:
+        return <Siete setStefp={setStep} />;
+      default:
+        return null;
+    }
+  };
+
+  return <div>{renderStep()}</div>;
+};
 
 export default Proyecto;
-
-
